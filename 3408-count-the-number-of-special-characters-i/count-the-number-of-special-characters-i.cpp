6d7e285d2dc91b31b381af3c2ed1d06e1cab86bd
AddAsher <109363338+AddAsher@users.noreply.github.com>
2026-05-25 22:37:53 -0400

    Time: 3 ms (29.00%) | Memory: 9.3 MB (47.56%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_set<char> s(word.begin(), word.end());
+        int count = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (s.count(c) && s.count(c - 32)) count++;
+        }
+        return count;       
+    } 
+};
