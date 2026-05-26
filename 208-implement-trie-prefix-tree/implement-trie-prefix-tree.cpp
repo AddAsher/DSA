04f7949bd775d7915c05fca16b68360ea03f8569
AddAsher <109363338+AddAsher@users.noreply.github.com>
2026-05-26 13:47:34 -0400

    Time: 20 ms (59.89%) | Memory: 50.6 MB (53.10%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,43 +1,44 @@
 class TrieNode{
 public:
-    TrieNode *child[26];
+    TrieNode* child[26];
     bool isWord;
     TrieNode(){
         isWord = false;
-        for (auto &a : child) a = nullptr;
+        for (auto &a: child) a = nullptr;
     }
 };
 
 class Trie {
     TrieNode* root;
 public:
-    Trie() {     
-        root = new TrieNode();
+    Trie() {
+        root = new TrieNode();        
     }
     
     void insert(string word) {
         TrieNode* p = root;
-        for (auto &a: word){
-            int i = a - 'a';
+        for(auto &c: word){
+            int i = c-'a';
             if(!p->child[i]) p->child[i] = new TrieNode();
             p = p->child[i];
         }
-        p->isWord= true;
+        p->isWord = true;
+        
     }
     
-    bool search(string word, bool prefix=false) {
+    bool search(string word, bool isEnd=true) {
         TrieNode* p = root;
-        for(auto &a: word){
-            int i = a-'a';
+        for(auto& c: word){
+            int i = c-'a';
             if(!p->child[i]) return false;
-            p = p->child[i];
+            p= p->child[i];
         }
-        if(prefix==false) return p->isWord;
-        return true;      
+        if(isEnd) return p->isWord;
+        return true;                
     }
     
     bool startsWith(string prefix) {
-        return search(prefix, true);
+        return search(prefix, false);
     }
 };
 
