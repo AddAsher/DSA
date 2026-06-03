70b2c978f32368e5ca4bcbdae716cbd4d4234f91
AddAsher <109363338+AddAsher@users.noreply.github.com>
2026-06-03 13:05:44 -0400

    Time: 34 ms (41.79%) | Memory: 106.7 MB (51.29%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -3,9 +3,9 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end(), less<int>());
         long long res = mass;
-        for(int i = 0; i<asteroids.size();i++){
-            if(res>=asteroids[i]){
-                res+= asteroids[i];
+        for(int as: asteroids){
+            if(res>=as){
+                res+= as;
             }else{
                 return false;
             }
