ece719973e2c1bcb67476ed818d19ce535d88b07
AddAsher <109363338+AddAsher@users.noreply.github.com>
2026-06-25 13:32:26 -0400

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.42%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long ans = 0;
+        int ans = 0;
         while(x!=0){
             if (ans > INT_MAX/10 || ans < INT_MIN/10) return 0;
             ans*=10;
